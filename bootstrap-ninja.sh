432399ceb904ff89d32bd626b4c21b69ac0751f6
kyawakyawa <kyawashell@gmail.com>
2020-10-02 22:26:43 +0900

    update cmake

--- bootstrap-ninja.sh
+++ bootstrap-ninja.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 rm -rf cmake-build-debug
+rm -rf cmake-build-release
 
 CC=clang CXX=clang++ cmake -Bcmake-build-debug -H. \
                            -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
                            -DCMAKE_BUILD_TYPE=Debug \
-                           -DUSE_CCACHE=ON \
-                           -DUSE_SANITIZER=OFF \
-                           -DUSE_STACK_TRACE_LOGGER=OFF \
                            -DBUILD_WITH_MARCH_NATIVE=ON \
-                           -GNinja
+                           -DUSE_CCACHE=ON \
+                           -DUSE_CPP20=ON \
+                           -DUSE_SANITIZER=OFF \
+                           -DUSE_STACK_TRACE_LOGGER=OFF
 
 mv cmake-build-debug/compile_commands.json .
+
+CC=clang CXX=clang++ cmake -Bcmake-build-release -H. \
+                           -DCMAKE_EXPORT_COMPILE_COMMANDS=0 \
+                           -DCMAKE_BUILD_TYPE=Release \
+                           -DBUILD_WITH_MARCH_NATIVE=ON \
+                           -DUSE_CCACHE=ON \
+                           -DUSE_CPP20=ON \
+                           -DUSE_SANITIZER=OFF \
+                           -DUSE_STACK_TRACE_LOGGER=OFF
