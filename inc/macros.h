10190ee0780794221f265d789c42a5af038af3a1
kyawakyawa <kyawashell@gmail.com>
2021-05-08 23:45:14 +0900

    fix

--- inc/macros.h
+++ inc/macros.h
@@ -85,19 +85,19 @@ SOFTWARE.
 
 #define CHECK_VARIANT_CONSTRAINTS(Variant, Concept) \
   template <Concept T> \
-  void _Check##Variant##Satisfies##Concept([[maybe_unused]] T t) {} \
+  struct _Check##Variant##Satisfies##Concept {}; \
   \
   template <size_t I> \
   void __Check##Variant##Satisfies##Concept() {\
-    typename std::variant_alternative<I, Variant>::type hoge = {}; \
-    _Check##Variant##Satisfies##Concept(hoge); \
+    [[maybe_unused]] \
+    _Check##Variant##Satisfies##Concept<typename std::variant_alternative<I, Variant>::type> hoge = {}; \
     __Check##Variant##Satisfies##Concept<I - 1>(); \
   } \
   \
   template <> \
   void __Check##Variant##Satisfies##Concept<0>() { \
-    std::variant_alternative<0, Variant>::type hoge = {}; \
-    _Check##Variant##Satisfies##Concept(hoge); \
+    [[maybe_unused]] \
+    _Check##Variant##Satisfies##Concept<typename std::variant_alternative<0, Variant>::type> hoge = {}; \
   } \
   \
   [[maybe_unused]] static void ___Check##Variant##Satisfies##Concept() { \
