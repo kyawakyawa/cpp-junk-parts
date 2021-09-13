1d8267a9f72682627b9cedcabdf26ba374707d66
kyawakyawa <kyawashell@gmail.com>
2021-09-13 10:58:06 +0900

    fix

--- script/do-clang-tidy.sh
+++ script/do-clang-tidy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# https://qiita.com/rtakasuke/items/fd35ef60370e3d8c225d
+set -e
+
 clang-tidy pc/*.cc --extra-arg=-std=c++20 --fix --fix-errors --header-filter=$(pwd)/inc/*.h
 clang-tidy pc/*.c --fix --fix-errors --header-filter=$(pwd)/inc/*.h
